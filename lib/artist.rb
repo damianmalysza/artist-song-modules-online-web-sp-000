0afb93f30ec743151df86ac6d800d5e92c20dcd4
Damian Malysza <damian.malysza@gmail.com>
2020-11-04 18:29:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,7 @@ require_relative '../config/environment'
 class Artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
+  include Paramable::InstanceMethods
 
   attr_accessor :name
   attr_reader :songs
